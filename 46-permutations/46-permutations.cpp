faa75c924c12114a9b063f5e26420e6ad3f72ce7
Abhinav-Vijay-Walunj <64826301+Abhinav-Vijay-Walunj@users.noreply.github.com>
2022-06-30 12:46:28 +0530

    Time: 4 ms (64.89%), Space: 7.7 MB (78.10%) - LeetHub

--- 46-permutations/46-permutations.cpp
+++ 46-permutations/46-permutations.cpp
@@ -1,36 +1,51 @@
 class Solution {
 public:
     
-    void solve(vector<int>& nums,vector<int>& ds,vector<vector<int>>& ans,int frq[])
+    void solve(int ind,vector<int>& nums,vector<vector<int>>& ans)
     {
-        if(ds.size() == nums.size())
+        if(ind == nums.size())
         {
-            ans.push_back(ds);
+            ans.push_back(nums);
             return;
         }
-        for(int i=0;i<nums.size();i++)
+        for(int i=ind;i<nums.size();i++)
         {
-            if(!frq[i])
-            {
-                ds.push_back(nums[i]);
-                frq[i]=1;
-                solve(nums,ds,ans,frq);
-                frq[i]=0;
-                ds.pop_back();
-            }
+            swap(nums[ind],nums[i]);
+            solve(ind+1,nums,ans);
+            swap(nums[ind],nums[i]);
         }
-        
     }
+//     void solve(vector<int>& nums,vector<int>& ds,vector<vector<int>>& ans,int frq[])
+//     {
+//         if(ds.size() == nums.size())
+//         {
+//             ans.push_back(ds);
+//             return;
+//         }
+//         for(int i=0;i<nums.size();i++)
+//         {
+//             if(!frq[i])
+//             {
+//                 ds.push_back(nums[i]);
+//                 frq[i]=1;
+//                 solve(nums,ds,ans,frq);
+//                 frq[i]=0;
+//                 ds.pop_back();
+//             }
+//         }
+        
+//     }
     vector<vector<int>> permute(vector<int>& nums) {
         vector<vector<int>> ans;
-        vector<int> ds;
-        int n=nums.size();
-        int frq[n];
-        for(int i=0;i<n;i++)
-        {
-            frq[i] = 0;
-        }
-        solve(nums,ds,ans,frq);
+        // vector<int> ds;
+        // int n=nums.size();
+        // int frq[n];
+        // for(int i=0;i<n;i++)
+        // {
+        //     frq[i] = 0;
+        // }
+        // solve(nums,ds,ans,frq);
+         solve(0,nums,ans);
         return ans;
         
     }
