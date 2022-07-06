4fbf1a22d96d81c9452e54cb9782098f038c05d2
Abhinav-Vijay-Walunj <64826301+Abhinav-Vijay-Walunj@users.noreply.github.com>
2022-07-06 19:45:54 +0530

    Time: 59 ms (55.43%), Space: 34.7 MB (66.07%) - LeetHub

---
+++ 700-search-in-a-binary-search-tree/700-search-in-a-binary-search-tree.cpp
@@ -0,0 +1,62 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    TreeNode* searchBST(TreeNode* root, int val) {
+      /*  
+        if(root == NULL)  
+        {
+            return root;
+        }
+        TreeNode* temp;
+        if(root->val == val)
+        {
+            temp = root;
+        }
+        else if(val<root->val)
+        {
+            searchBST(root->right,val);
+        }
+        else
+        {
+            searchBST(root->left,val);
+        }
+    
+    
+    return temp;
+    }
+        */
+        if(!root) return root;
+        //create node to return 
+        TreeNode *node=new TreeNode(); 
+        // if root->val != val,search in left and right
+        //otherwise this would be required node and we would return it
+        if(val<root->val){
+            //search in left 
+            node=searchBST(root->left,val);
+        } 
+        else if(val>root->val){
+            //search in right
+            node=searchBST(root->right,val);
+        } 
+        else {
+            //required node
+            node=root;
+        }
+        return node;
+        
+    }
+            
+        
+        
+    
+};
