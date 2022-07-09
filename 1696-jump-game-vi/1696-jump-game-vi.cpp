a64da0b95837d15bde64fd655b7a0207e6ec3832
Abhinav-Vijay-Walunj <64826301+Abhinav-Vijay-Walunj@users.noreply.github.com>
2022-07-09 22:07:16 +0530

    Time: 484 ms (16.40%), Space: 90.8 MB (43.66%) - LeetHub

---
+++ 1696-jump-game-vi/1696-jump-game-vi.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    #define pii pair<int, int>
+    int maxResult(vector<int>& nums, int k) {
+        int n=nums.size();
+        int score[n];
+        
+        priority_queue<pii> pq;
+        
+        for(int i = n-1;i>=0;i--)
+        {
+            while(pq.size()&&pq.top().second>i+k)
+            {
+                pq.pop();
+            }
+            score[i]=nums[i];
+            score[i]+=(pq.size()?pq.top().first:0);
+            
+            pq.push({score[i],i});
+        }
+         return score[0];   
+        
+    }
+};
